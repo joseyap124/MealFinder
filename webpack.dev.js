631975e237dfa656120e5e3cabdbe724cdf9d1e3
Ananda Dwi Prayoga <43091969+AnandaDwiprayoga@users.noreply.github.com>
2020-08-10 16:26:45 +0700

    Update webpack.dev.js
    babel-loader seharusnya berada pada konfigurasi producton

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,22 +3,5 @@ const common = require("./webpack.common.js");
  
  
 module.exports = merge(common, {
-    mode: "production",
-    module: {
-        rules: [
-            /* babel loader */
-            {
-                test: /\.js$/,
-                exclude: "/node_modules/",
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"]
-                        }
-                    }
-                ]
-            }
-        ]
-    }
+    mode: "production"
 })
