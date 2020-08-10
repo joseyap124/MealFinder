52088fcac87774da0849db63fcd129ed52e5353c
Ananda Dwi Prayoga <43091969+AnandaDwiprayoga@users.noreply.github.com>
2020-08-10 16:25:53 +0700

    Update webpack.prod.js
    add babe-loader to production

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,4 +4,21 @@ const common = require("./webpack.common.js");
  
 module.exports = merge(common, {
     mode: "development",
+    module: {
+        rules: [
+            /* babel loader */
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
 })
